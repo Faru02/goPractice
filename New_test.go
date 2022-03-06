9bfdcc3e8c7e7804c708a5fbfeea8b5b64d920a3
Faru02 <faru02135@gmail.com>
2022-03-05 19:10:01 -0800

    change commit again

--- New_test.go
+++ New_test.go
@@ -8,7 +8,9 @@ func main(){
 
        fmt.Println ( "gitpush")     
        fmt.Println ( "try to learn code")
+      fmt.Println (" change commit try ")
 
+      fmt.Println(" takim is abdur son")
 
 
 
