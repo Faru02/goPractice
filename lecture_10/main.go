71a933fe65105a81fb58a604b33fe2f1a6aebf36
Faru02 <faru02135@gmail.com>
2022-03-07 20:02:08 -0800

    file

---
+++ lecture_10/main.go
@@ -0,0 +1,22 @@
+package main 
+
+import "fmt"
+//import "reflect"
+
+func main(){
+
+ x := make(map[string]string)
+  x["name"]= "Farhan"
+  x["age"]= "19"
+  x["number"]= "02135"
+
+
+   fmt.Println(x["number"])
+
+
+
+
+
+
+
+   }
